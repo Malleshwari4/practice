b888bd0138ab9d460ceca92a8dfc2eafef74f8a9
Malleshwari4 <pallikondamalleshwari@gmail.com>
2025-04-23 12:29:03 +1300

    first commit

---
+++ src/test/java/practicemaven/Class1.java
@@ -0,0 +1,31 @@
+package practicemaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class1 {
+	WebDriver driver;
+	
+	@Test(priority=1)
+	public void browserlaunch() {
+		driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+	}
+	
+	@Test(priority=2)
+	public void vadilation() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@Test(priority=3)
+	public void logout() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.close();
+	}
+}
